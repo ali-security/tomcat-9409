41456076960a1de798f41ef6db4b8ad035a1a6a7
Tal Samucha <tal.s@sealsecurity.io>
2025-08-24 10:26:45 +0300

    CVE-2025-24813

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -596,7 +596,7 @@ public class DefaultServlet extends HttpServlet {
         }
 
         InputStream resourceInputStream = null;
-
+        File tempContentFile = null;
         try {
             // Append data specified in ranges to existing content for this
             // resource - create a temp. file on the local filesystem to
@@ -605,8 +605,8 @@ public class DefaultServlet extends HttpServlet {
             if (range == IGNORE) {
                 resourceInputStream = req.getInputStream();
             } else {
-                File contentFile = executePartialPut(req, range, path);
-                resourceInputStream = new FileInputStream(contentFile);
+                tempContentFile = executePartialPut(req, range, path);
+                resourceInputStream = new FileInputStream(tempContentFile);
             }
 
             if (resources.write(path, resourceInputStream, true)) {
@@ -625,6 +625,9 @@ public class DefaultServlet extends HttpServlet {
                 } catch (IOException ioe) {
                     // Ignore
                 }
+            }
+            if (tempContentFile != null) {
+                tempContentFile.delete();
             }
         }
     }
@@ -648,13 +651,7 @@ public class DefaultServlet extends HttpServlet {
         // resource - create a temp. file on the local filesystem to
         // perform this operation
         File tempDir = (File) getServletContext().getAttribute(ServletContext.TEMPDIR);
-        // Convert all '/' characters to '.' in resourcePath
-        String convertedResourcePath = path.replace('/', '.');
-        File contentFile = new File(tempDir, convertedResourcePath);
-        if (contentFile.createNewFile()) {
-            // Clean up contentFile when Tomcat is terminated
-            contentFile.deleteOnExit();
-        }
+        File contentFile = File.createTempFile("put-part-", null, tempDir);
 
         try (RandomAccessFile randAccessContentFile = new RandomAccessFile(contentFile, "rw")) {
 
